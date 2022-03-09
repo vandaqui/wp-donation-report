f4e491d5f61638ec6ecfe2fa05cb9a1e62089d8a
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2022-03-09 11:15:49 -0300

    Add plugin info

--- donation-report/settings.php
+++ donation-report/settings.php
@@ -1,1 +1,10 @@
 <?php
+
+/*
+  Plugin Name: Donation Report
+  Description: Display a list of donations made by your team
+  Version: 1.0
+  Author: Vand Aquino
+  Author URI: https://www.vandaquino.com.br/
+*/
+  
