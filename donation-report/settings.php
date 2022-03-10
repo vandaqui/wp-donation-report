cd0a279f0df7eb78777ee484a96876066474f0e1
Vand Aquino <64815489+vandaqui@users.noreply.github.com>
2022-03-10 10:36:47 -0300

    Update settings.php

--- donation-report/settings.php
+++ donation-report/settings.php
@@ -8,3 +8,12 @@
   Author URI: https://www.vandaquino.com.br/
 */
   
+add_action ('admin_menu', 'wpDReportSettings');
+/* 5 Arguments: 1-Title of the Page, 2-Menu Display Name, 3-Permissions/Capabilities, 4-Shortname/Slug, 5-functionHTML */
+function wpDReportSettings(){
+  add_options_page('Donation Report', 'DReport', 'manage_options', 'wp-dreport', 'dreportHTML');
+}
+
+function dreportHTML(){
+  
+}
